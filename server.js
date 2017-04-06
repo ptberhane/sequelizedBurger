39d3a56acae3f0d54ec2b34398173b67b96e6090
ptberhane <piniel.berhane@gmail.com>
2017-04-06 08:21:42 -0400

    files updated

---
+++ server.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var bodyParser= require("body-parser");
+app.use(methodOverride("_method"));
+var exphbs = require("express-handlebars");
+
+var app= express();
+
+var port = 3000;
+
+
+
+app.use(express.static(process.cwd() + "/views"));
+
+app.get('/', function (req, res, next) {
+  res.render('index');
+});
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
+
+const db = require("./models");
+app.get('/burgers', function(req, res, next) {
+  console.log("Called burgers")
+
+  res.render('index');
+});
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+
+
+app.listen(port, function() {
+  console.log("Listening on PORT " + port);
+});
